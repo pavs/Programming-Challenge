9f179b0a918e9874ac3249934c4b28483b58c739
Pavel Mah <pavs.ma@hotmail.com>
2014-01-06 15:06:27 +0600

    Challenge 4 finished

---
+++ Challenges/4/4-nextprime.php
@@ -0,0 +1,32 @@
+<?php
+$primes = array(
+      2,   3,   5,   7,  11,  13,  17,  19,  23,  29, 
+     31,  37,  41,  43,  47,  53,  59,  61,  67,  71, 
+     73,  79,  83,  89,  97, 101, 103, 107, 109, 113, 
+    127, 131, 137, 139, 149, 151, 157, 163, 167, 173 
+); // add more primes to it
+
+if(array_keys($_POST) === array("hid", "sub")) {
+		$num = ($_POST["hid"] == sizeof($primes)-1) ? $_POST["hid"] : $_POST["hid"]+1;
+	}
+
+else {
+		$num = 0;
+	}
+?>
+
+
+<html>
+<head>
+	Prints Next Prime Number
+</head>
+<body>
+
+<form method="POST">
+<h3><?php print($primes[$num]); ?></h3>
+<input type="hidden" name="hid" value="<?php print($num); ?>"/>
+<input type="submit" name="sub" value="Next Prime"/>
+</form>
+
+</body>
+</html>
